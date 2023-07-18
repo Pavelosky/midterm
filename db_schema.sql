4ed4408cf2a6fc10980ef08cbead404f3d3f5cc4
Pavelosky <pawel.bialkowskigru@gmail.com>
2023-07-18 06:26:08 +0200

    added form to create new user

--- db_schema.sql
+++ db_schema.sql
@@ -9,10 +9,10 @@ BEGIN TRANSACTION;
 
 CREATE TABLE IF NOT EXISTS Users (
     user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_name TEXT NOT NULL,
+    username TEXT NOT NULL,
     email TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL,
-    is_author INTEGER DEFAULT 0
+    is_author TEXT DEFAULT 'No'
 );
 
 CREATE TABLE IF NOT EXISTS Articles (
@@ -32,12 +32,12 @@ CREATE TABLE IF NOT EXISTS Comments (
     comment TEXT NOT NULL,
     comment_date CURRENT_TIMESTAMP,
     user_id  INT, --the user that the record belongs to
-    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+    FOREIGN KEY (article_id) REFERENCES Articles(article_id)
 );
 
 --insert default data (if necessary here)
 
-INSERT INTO Users ('user_name','email','password', 'is_author') VALUES ('Admin','admin@blog.com','password123',1);
+INSERT INTO Users ('username','email','password', 'is_author') VALUES ('Admin','admin@blog.com','password123','Yes');
 INSERT INTO Articles ('title','subtitle','content','publication_date', 'user_id') VALUES('This is a title of an article', 'Some more info about the article', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', DATE('now'), 1); --try changing the user_id to a different number and you will get an error
 
 COMMIT;
--- index.js
+++ index.js
@@ -23,7 +23,21 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
   // Render the home.ejs file
   res.render('home');
-})
+  })
+
+  app.post("/home", function (req,res) {
+    // saving data in database
+    let sqlquery = "INSERT INTO Users (user_name, password, email, is_author) VALUES (?,?,?,?)";
+    // execute sql query
+    let newrecord = [req.body.username, req.body.password, req.body.email, req.body.is_author,];
+    db.query(sqlquery, newrecord, (err, result) => {
+    if (err) {
+    return console.error(err.message);
+    }else
+    res.send("Your account has been created, <br> <b>Username:</b> "+ req.body.username + "<br><b> Password: </b>"+ req.body.password +" <br><b> email: </b>"+ req.body.email +
+    "<br><b>Author:</b> "+ req.body.is_author)
+    });
+    });
 
 app.get('/article', (req, res) => {
   // Render the home.ejs file
